3157ebca959c36773e5c7f8403cb45cc9c461d54
jp-knj <kenji.tomita1996@gmail.com>
2022-06-04 21:24:48 +0900

    :shirt: enforces a consistent code style using prettier

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -22,40 +22,40 @@ Primary.args = {
   is_primary: true,
   label: 'Button',
 };
-Primary.play = async ({canvasElement, args}): Promise<void> => {
-    const canvas = within(canvasElement);
-    await userEvent.click(canvas.getByRole('button'));
-    await expect(args.onClick).toHaveBeenCalled();
-}
+Primary.play = async ({ canvasElement, args }): Promise<void> => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole('button'));
+  await expect(args.onClick).toHaveBeenCalled();
+};
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   label: 'Button',
 };
-Secondary.play = async ({canvasElement, args}): Promise<void> => {
-    const canvas = within(canvasElement);
-    await userEvent.click(canvas.getByRole('button'));
-    await expect(args.onClick).toHaveBeenCalled();
-}
+Secondary.play = async ({ canvasElement, args }): Promise<void> => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole('button'));
+  await expect(args.onClick).toHaveBeenCalled();
+};
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
   label: 'Button',
 };
-Large.play = async ({canvasElement, args}): Promise<void> => {
-    const canvas = within(canvasElement);
-    await userEvent.click(canvas.getByRole('button'));
-    await expect(args.onClick).toHaveBeenCalled();
-}
+Large.play = async ({ canvasElement, args }): Promise<void> => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole('button'));
+  await expect(args.onClick).toHaveBeenCalled();
+};
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
   label: 'Button',
 };
-Small.play = async ({canvasElement, args}): Promise<void> => {
-    const canvas = within(canvasElement);
-    await userEvent.click(canvas.getByRole('button'));
-    await expect(args.onClick).toHaveBeenCalled();
-}
+Small.play = async ({ canvasElement, args }): Promise<void> => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole('button'));
+  await expect(args.onClick).toHaveBeenCalled();
+};
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -22,7 +22,7 @@ type ButtonProps = {
    * Optional click handler
    */
   onClick?: () => void;
-}
+};
 
 const button = css`
   font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
@@ -32,48 +32,53 @@ const button = css`
   cursor: pointer;
   display: inline-block;
   line-height: 1;
-`
+`;
 const primary = css`
   color: white;
   background-color: #1ea7fd;
-`
+`;
 
 const secondary = css`
   color: #333;
   background-color: transparent;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
-`
+`;
 
 const small = css`
   font-size: 12px;
   padding: 10px 16px;
-`
+`;
 
 const medium = css`
   font-size: 14px;
   padding: 11px 20px;
-`
+`;
 
 const large = css`
   font-size: 16px;
   padding: 12px 24px;
-`
+`;
 
 /**
  * Primary UI component for user interaction
  */
-export const Button:React.FC<ButtonProps> = ({
+export const Button: React.FC<ButtonProps> = ({
   is_primary = false,
   size = 'medium',
   backgroundColor,
   label,
   ...otherProps
-}):JSX.Element => {
+}): JSX.Element => {
   const mode = is_primary ? primary : secondary;
 
-  const sizeCss = size === 'small' ? small
-      : size === 'medium' ? medium
-          : size === 'large' ? large : '';
+  const sizeCss =
+    size === 'small'
+      ? small
+      : size === 'medium'
+      ? medium
+      : size === 'large'
+      ? large
+      : '';
   return (
     <button
       type="button"
