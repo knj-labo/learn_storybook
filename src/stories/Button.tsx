45ae3e52fc3b64b9d208612c712856b5c9839762
jp-knj <kenji.tomita1996@gmail.com>
2022-06-04 22:21:51 +0900

    :art: avoid error and enforce conventions

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -24,14 +24,14 @@ type ButtonProps = {
   onClick?: () => void;
 };
 
-const button = css`
-  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+const button = () => css`
+  display: inline-block;
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
   font-weight: 700;
+  line-height: 1;
+  cursor: pointer;
   border: 0;
   border-radius: 3em;
-  cursor: pointer;
-  display: inline-block;
-  line-height: 1;
 `;
 const primary = css`
   color: white;
@@ -41,22 +41,22 @@ const primary = css`
 const secondary = css`
   color: #333;
   background-color: transparent;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  box-shadow: rgb(0 0 0 / 15%) 0 0 0 1px inset;
 `;
 
 const small = css`
-  font-size: 12px;
   padding: 10px 16px;
+  font-size: 12px;
 `;
 
 const medium = css`
-  font-size: 14px;
   padding: 11px 20px;
+  font-size: 14px;
 `;
 
 const large = css`
-  font-size: 16px;
   padding: 12px 24px;
+  font-size: 16px;
 `;
 
 /**
