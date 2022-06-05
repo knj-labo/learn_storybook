ba7f0d2e1aaf32e69659b5e66c6549d9a8b44b1a
jp-knj <kenji.tomita1996@gmail.com>
2022-06-05 21:26:04 +0900

    :green_heart: add interaction test with form

--- src/shared/Form/index.stories.tsx
+++ src/shared/Form/index.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import { expect } from '@storybook/jest';
+import { within, screen, userEvent } from '@storybook/testing-library';
 import { Form } from '../Form';
 
 export default {
@@ -9,4 +10,20 @@ export default {
 } as ComponentMeta<typeof Form>;
 
 const Template: ComponentStory<typeof Form> = () => <Form />;
-export const Primary = Template.bind({});
+
+export const 全項目適切に入力された場合 = Template.bind({});
+全項目適切に入力された場合.play = async ({ canvasElement }) => {
+  {
+    const canvas = within(canvasElement);
+    await userEvent.type(canvas.getByTestId('name'), 'サンプル太郎', {
+      delay: 100, // 文字が入力される間隔の指定
+    });
+    await userEvent.type(canvas.getByTestId('email'), 'sample@email.com', {
+      delay: 100,
+    });
+    await userEvent.type(canvas.getByTestId('password'), 'samplepassword', {
+        delay: 100,
+    });
+    await userEvent.click(canvas.getByRole('button'));
+  }
+};
--- src/shared/Form/index.tsx
+++ src/shared/Form/index.tsx
@@ -1,60 +1,49 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+// import { css } from '@emotion/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Props = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 export const Form = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const handleRegistration = (data: unknown) => console.log(data);
-
-  const handleError = (errors: unknown) => {};
-
-  const registerOptions = {
-    name: { required: 'Name is required' },
-    email: { required: 'Email is required' },
-    password: {
-      required: 'Password is required',
-      minLength: {
-        value: 8,
-        message: 'Password must have at least 8 characters',
-      },
-    },
+  const { register, handleSubmit } = useForm<Props>();
+  const onSubmit = (data: unknown): void => {
+    alert(JSON.stringify(data));
   };
 
-  const textInputOfName = register('name', registerOptions.name);
-  const textInputOfEmail = register('name', registerOptions.email);
-  const textInputOfPassword = register('name', registerOptions.password);
-
   return (
-    <form onSubmit={handleSubmit(handleRegistration, handleError)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="name">
-          Name
-          <input id="name" type="text" {...textInputOfName} />
-        </label>
-        <small className="text-danger">
-          {errors?.name && errors.name.message}
-        </small>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          placeholder="bill"
+          {...register('name')}
+          data-testid="name"
+        />
       </div>
+
       <div>
-        <label htmlFor="email">
-          Email
-          <input id="email" type="email" {...textInputOfEmail} />
-        </label>
-        <small className="text-danger">
-          {errors?.email && errors.email.message}
-        </small>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          placeholder="luo"
+          {...register('email')}
+          data-testid="email"
+        />
       </div>
+
       <div>
-        <label htmlFor="password">
-          Password
-          <input id="password" type="password" {...textInputOfPassword} />
-        </label>
-        <small className="text-danger">
-          {errors?.password && errors.password.message}
-        </small>
+        <label htmlFor="email">Password</label>
+        <input
+          placeholder="bluebill1049@hotmail.com"
+          type="password"
+          {...register('password')}
+          data-testid="password"
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
