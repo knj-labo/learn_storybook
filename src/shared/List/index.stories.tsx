cec5734501e4aa5feca719d54dfd196cb9edd056
jp-knj <kenji.tomita1996@gmail.com>
2022-06-13 20:53:29 +0900

    :sparkles: update list components

---
+++ src/shared/List/index.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { List } from '../List';
+
+export default {
+    title: 'Shared/List',
+    component: List,
+} as ComponentMeta<typeof List>;
+
+const Template: ComponentStory<typeof List> = () => <List />;
+
+export const Demo = Template.bind({});
+Demo.args = {}
---
+++ src/shared/List/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const list = css`
+  list-style: none;
+  padding: 8px;
+  border-width: 2px;
+  border-style: solid;
+  border-color: #222222;
+  
+  > * + * {
+    margin-top: 8px;
+  }
+`
+
+const item = css`
+    padding: 8px
+`
+
+const link = css`
+  text-decoration: none;
+  color: #222222;
+  
+  :hover {
+    border-bottom: solid 2px #222;
+  }
+`
+
+export const List = () => {
+    return (
+        <ul css={list}>
+            <li css={item}><a css={link} href="#">Twitter</a></li>
+            <li css={item}><a css={link} href="#">Github</a></li>
+            <li css={item}><a css={link} href="#">Zenn</a></li>
+            <li css={item}><a css={link} href="#">Note</a></li>
+        </ul>
+    )
+};
